76e7dd4406693d3ea840d2fd2f5f65325aab36a7
Chris Cashwell <ccashwell@gmail.com>
2024-02-20 17:00:58 -0500

    feat: simple allowlist

--- src/allowlists/AllowList.sol
+++ src/allowlists/AllowList.sol
@@ -1,10 +1,12 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.24;
 
+import {Ownable} from "lib/solady/src/auth/Ownable.sol";
+
 /// @title Boost AllowList
 /// @notice Abstract contract for a generic Allow List within the Boost protocol
 /// @dev Allow List classes are expected to implement the authorization of users based on implementation-specific criteria, which may involve validation of a data payload. If no data is required, calldata should be empty.
-abstract contract AllowList {
+abstract contract AllowList is Ownable {
     /// @notice Check if a user is authorized
     /// @param user_ The address of the user
     /// @param data_ The data payload for the authorization check, if applicable
---
+++ src/allowlists/SimpleAllowList.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.24;
+
+import {AllowList} from "./AllowList.sol";
+
+/// @title Simple AllowList
+/// @notice A simple implementation of an AllowList that checks if a user is authorized based on a list of allowed addresses
+contract SimpleAllowList is AllowList {
+    error LengthMismatch();
+
+    mapping(address => bool) public allowed;
+
+    /// @notice Check if a user is authorized
+    /// @param user_ The address of the user
+    /// @param - The data payload for the authorization check, not used in this implementation
+    /// @return True if the user is authorized
+    function isAllowed(
+        address user_,
+        bytes calldata /* data_ - unused */
+    ) external view override returns (bool) {
+        return allowed[user_];
+    }
+
+    /// @notice Set the allowed status of a user
+    /// @param users_ The list of users to update
+    /// @param allowed_ The allowed status of each user
+    function setAllowed(
+        address[] calldata users_,
+        bool[] calldata allowed_
+    ) external onlyOwner {
+        if (users_.length != allowed_.length) {
+            revert LengthMismatch();
+        }
+
+        for (uint256 i = 0; i < users_.length; i++) {
+            allowed[users_[i]] = allowed_[i];
+        }
+    }
+}

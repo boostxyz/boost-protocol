ffbc849e837d3da6a8ea8b3ecd2c4a64089494eb
Chris Cashwell <ccashwell@gmail.com>
2024-02-21 14:07:18 -0500

    chore: update abstract Action behavior

--- src/actions/Action.sol
+++ src/actions/Action.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.24;
 
+import {Validator} from "../validators/Validator.sol";
+
 /// @title Boost Action
 /// @notice Abstract contract for a generic Action within the Boost protocol
 /// @dev Action classes are expected to decode the calldata for implementation-specific handling. If no data is required, calldata should be empty.
@@ -14,16 +16,21 @@ abstract contract Action {
         bytes data
     );
 
+    /// @notice Thrown when the `execute` function is not implemented
+    error ExecuteNotImplemented();
+
     /// @notice Emitted when the action is validated
     /// @dev The `data` field should contain implementation-specific context, if applicable.
     event ActionValidated(address indexed user, bool isValidated, bytes data);
+
+    Validator public immutable VALIDATOR;
 
     /// @notice Execute the action
     /// @param data_ The data payload for the action
     /// @return (success, data) A tuple of the success status and the returned data
     function execute(
         bytes calldata data_
-    ) external virtual returns (bool, bytes memory);
+    ) external payable virtual returns (bool, bytes memory);
 
     /// @notice Prepare the action for execution and return the expected payload
     /// @param data_ The data payload for the action
@@ -32,12 +39,9 @@ abstract contract Action {
         bytes calldata data_
     ) external virtual returns (bytes memory);
 
-    /// @notice Validate the action for the user
-    /// @param user_ The user to validate the action for
+    /// @notice Validate that the action has been completed successfully
     /// @param data_ The data payload for the action
     /// @return True if the action has been validated for the user
-    function validate(
-        address user_,
-        bytes calldata data_
-    ) external virtual returns (bool);
+    /// @dev It is the responsibility of the validator to unpack the data and perform the validation
+    function validate(bytes calldata data_) external virtual returns (bool);
 }

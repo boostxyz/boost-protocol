19abaeb7129c76fef92bfd2a98177ca3de5bb828
Chris Cashwell <ccashwell@gmail.com>
2024-02-29 20:08:24 -0500

    feat(action): ERC721MintAction

--- src/actions/ERC721MintAction.sol
+++ src/actions/ERC721MintAction.sol
@@ -1,73 +1,95 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.24;
 
-import {Action} from "./Action.sol";
-import {Validator} from "../validators/Validator.sol";
+import {console} from "lib/forge-std/src/console.sol";
+
+import {LibZip} from "lib/solady/src/utils/LibZip.sol";
+import {ERC721} from "lib/solady/src/tokens/ERC721.sol";
+
+import {Cloneable} from "src/Cloneable.sol";
+import {Action} from "src/actions/Action.sol";
+import {Validator} from "src/validators/Validator.sol";
 
 /// @title ERC721 Mint Action
-/// @notice A primitive action to mint an ERC721 token
+/// @notice A primitive action to mint and/or validate that an ERC721 token has been minted
 /// @dev The action is expected to be prepared with the data payload for the minting of the token
 /// @dev This a minimal generic implementation that should be extended if additional functionality or customizations are required
 /// @dev It is expected that the target contract has an externally accessible mint function whose selector
-contract ERC721MintAction is Action {
-    error MintFailed();
+contract ERC721MintAction is Action, Validator {
+    using LibZip for bytes;
 
     /// @notice The target ERC721 contract
     /// @dev This is the contract against which the mint action should be executed
-    address public immutable MINT_TARGET;
+    address public target;
 
     /// @notice The selector for the mint function
-    /// @dev This is expected to be `bytes4(keccak256(functionSignature))`
-    bytes4 public immutable MINT_SELECTOR;
+    /// @dev This is expected to be the actual selector (e.g. `bytes4(keccak256("myMintFunction(address,uint256)"))`)
+    bytes4 public selector;
 
-    /// @notice The value to send with the mint function
+    /// @notice The native token value to send with the mint function
     /// @dev This value is expected to be the value required for the mint function on the target contract
-    uint256 public immutable MINT_VALUE;
+    uint256 public value;
+
+    /// @notice The set of validated tokens
+    /// @dev This is intended to prevent multiple validations against the same token ID
+    mapping(uint256 => bool) public validated;
 
     /// @notice Construct the ERC721 Mint Action
-    /// @param validator_ The address of the validator contract
-    /// @param target_ The address of the ERC721 contract
-    /// @param mintSelector_ The selector for the mint function
-    /// @param mintValue_ The value to send with the mint function
-    /// @dev The `mintSelector_` and `mintValue_` are expected to be the selector and value required for the mint function on the target contract
-    constructor(
-        Validator validator_,
-        address target_,
-        bytes4 mintSelector_,
-        uint256 mintValue_
-    ) {
-        VALIDATOR = validator_;
-        MINT_TARGET = target_;
-        MINT_SELECTOR = mintSelector_;
-        MINT_VALUE = mintValue_;
+    /// @dev Because this contract is a base implementation, it should not be initialized through the constructor. Instead, it should be cloned and initialized using the {initialize} function.
+    constructor() {
+        _disableInitializers();
+    }
+
+    /// @inheritdoc Cloneable
+    /// @notice Initialize the contract with the owner and the required mint data
+    /// @param data_ The data payload for the mint action `(address target, bytes4 selector, uint256 value)`
+    function initialize(bytes calldata data_) external override initializer {
+        (address target_, bytes4 selector_, uint256 value_) =
+            abi.decode(data_.cdDecompress(), (address, bytes4, uint256));
+        target = target_;
+        selector = selector_;
+        value = value_;
+        _initializeOwner(msg.sender);
     }
 
     /// @notice Execute the action (not yet implemented)
-    /// @param - The data payload for the call
-    /// @return (success, returnData) The success status of the call
-    function execute(
-        bytes calldata /* data_ */
-    ) external payable override returns (bool, bytes memory) {
+    /// @param - The data payload for the call (not used in this implementation)
+    /// @return success The success status of the call
+    /// @return returnData The return data from the call
+    function execute(bytes calldata /* data_ */ )
+        external
+        payable
+        override
+        returns (bool success, bytes memory returnData)
+    {
         revert ExecuteNotImplemented();
     }
 
     /// @notice Prepare the action for execution and return the expected payload
-    /// @param data_ The data payload for the action
-    /// @return The encoded payload for the mint function
+    /// @param data_ The ABI-encoded payload for the action `(address recipient)`
+    /// @return The encoded payload to be sent with the call
     /// @dev Note that the mint value is NOT included in the prepared payload but must be sent with the call
-    function prepare(
-        bytes calldata data_
-    ) public view override returns (bytes memory) {
-        return abi.encodeWithSelector(MINT_SELECTOR, data_);
+    function prepare(bytes calldata data_) public view override returns (bytes memory) {
+        (address recipient) = abi.decode(data_, (address));
+        return abi.encodeWithSelector(selector, recipient);
     }
 
-    /// @inheritdoc Action
+    /// @inheritdoc Validator
     /// @notice Validate that the action has been completed successfully
-    /// @param data_ The data payload for the action
-    /// @return True if the action has been validated for the user
-    function validate(
-        bytes calldata data_
-    ) external virtual override returns (bool) {
-        return VALIDATOR.validate(data_);
+    /// @param data_ The data payload for the action `(address holder, uint256 tokenId)`
+    /// @return success True if the action has been validated for the user
+    function validate(bytes calldata data_) external virtual override(Action, Validator) returns (bool success) {
+        (address holder, uint256 tokenId) = abi.decode(data_.cdDecompress(), (address, uint256));
+        if (ERC721(target).ownerOf(tokenId) == holder && !validated[tokenId]) {
+            validated[tokenId] = true;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /// @inheritdoc Cloneable
+    function supportsInterface(bytes4 interfaceId) public view virtual override(Action, Validator) returns (bool) {
+        return super.supportsInterface(interfaceId);
     }
 }
---
+++ test/actions/ERC721MintAction.t.sol
@@ -0,0 +1,212 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.24;
+
+import {Test, console} from "lib/forge-std/src/Test.sol";
+
+import {ERC721} from "lib/solady/src/tokens/ERC721.sol";
+import {LibClone} from "lib/solady/src/utils/LibClone.sol";
+import {LibZip} from "lib/solady/src/utils/LibZip.sol";
+
+import {Action} from "src/actions/Action.sol";
+import {ERC721MintAction} from "src/actions/ERC721MintAction.sol";
+import {Validator} from "src/validators/Validator.sol";
+
+contract MockERC721 is ERC721 {
+    uint256 public totalSupply;
+    uint256 public mintPrice = 0.1 ether;
+
+    function name() public pure override returns (string memory) {
+        return "MockERC721";
+    }
+
+    function symbol() public pure override returns (string memory) {
+        return "MOCK";
+    }
+
+    function mint(address to) public payable {
+        require(msg.value >= mintPrice, "MockERC721: gimme more money!");
+        console.log("Minting token for", to, "with", msg.value);
+        // pre-increment so IDs start at 1
+        _mint(to, ++totalSupply);
+    }
+
+    function tokenURI(uint256 id) public view virtual override returns (string memory) {
+        return string(abi.encodePacked("https://example.com/token/", id));
+    }
+}
+
+contract ERC721MintActionTest is Test {
+    using LibZip for bytes;
+
+    ERC721MintAction public action;
+    MockERC721 public mockAsset = new MockERC721();
+
+    function setUp() public {
+        action = _newActionClone();
+    }
+
+    /////////////////////////////////
+    // ERC721MintAction.initialize //
+    /////////////////////////////////
+
+    function testInitialize() public {
+        // Initialize the ERC721MintAction
+        _initialize(address(mockAsset), MockERC721.mint.selector, mockAsset.mintPrice());
+
+        // Ensure the action was initialized correctly
+        assertEq(action.target(), address(mockAsset));
+        assertEq(action.selector(), bytes4(keccak256("mint(address)")));
+        assertEq(action.value(), 0.1 ether);
+    }
+
+    //////////////////////////////
+    // ERC721MintAction.execute //
+    //////////////////////////////
+
+    function testExecute() public {
+        // Initialize the ERC721MintAction
+        _initialize(address(mockAsset), MockERC721.mint.selector, mockAsset.mintPrice());
+
+        // Execute is not implemented => revert
+        vm.expectRevert(Action.ExecuteNotImplemented.selector);
+        action.execute("");
+    }
+
+    //////////////////////////////
+    // ERC721MintAction.prepare //
+    //////////////////////////////
+
+    function testPrepare() public {
+        // Initialize the ERC721MintAction
+        _initialize(address(mockAsset), MockERC721.mint.selector, mockAsset.mintPrice());
+
+        // Prepare the action
+        bytes memory payload = action.prepare(abi.encode(address(1)));
+        assertEq(payload, abi.encodeWithSelector(MockERC721.mint.selector, address(1)));
+    }
+
+    function testPrepare_SendPayload() public {
+        // Initialize the ERC721MintAction
+        _initialize(address(mockAsset), MockERC721.mint.selector, mockAsset.mintPrice());
+
+        // Prepare the action
+        bytes memory payload = action.prepare(abi.encode(address(0xdeadbeef)));
+        (bool success, bytes memory returnData) = address(mockAsset).call{value: mockAsset.mintPrice()}(payload);
+        assertTrue(success);
+        assertEq(returnData.length, 0);
+        assertEq(mockAsset.totalSupply(), 1);
+        assertEq(mockAsset.ownerOf(1), address(0xdeadbeef));
+    }
+
+    ///////////////////////////////
+    // ERC721MintAction.validate //
+    ///////////////////////////////
+
+    function testValidate() public {
+        // Initialize the ERC721MintAction
+        _initialize(address(mockAsset), MockERC721.mint.selector, mockAsset.mintPrice());
+
+        // Mint a token
+        mockAsset.mint{value: mockAsset.mintPrice()}(address(this));
+        assertTrue(mockAsset.ownerOf(1) == address(this));
+
+        // Validate the action
+        assertTrue(action.validate(LibZip.cdCompress(abi.encode(address(this), 1))));
+    }
+
+    function testValidate_WrongHolder() public {
+        // Initialize the ERC721MintAction
+        _initialize(address(mockAsset), MockERC721.mint.selector, mockAsset.mintPrice());
+
+        // Mint a token
+        mockAsset.mint{value: mockAsset.mintPrice()}(address(this));
+        assertTrue(mockAsset.ownerOf(1) == address(this));
+
+        // Validate the action with an invalid holder
+        assertFalse(action.validate(LibZip.cdCompress(abi.encode(address(0xdeadbeef), 1))));
+    }
+
+    function testValidate_AlreadyValidated() public {
+        // Initialize the ERC721MintAction
+        _initialize(address(mockAsset), MockERC721.mint.selector, mockAsset.mintPrice());
+
+        // Mint a token
+        mockAsset.mint{value: mockAsset.mintPrice()}(address(this));
+        assertTrue(mockAsset.ownerOf(1) == address(this));
+
+        // Validate the action
+        assertTrue(action.validate(LibZip.cdCompress(abi.encode(address(this), 1))));
+
+        // Validate the action again => false
+        assertFalse(action.validate(LibZip.cdCompress(abi.encode(address(this), 1))));
+    }
+
+    function testValidate_NonExistentToken() public {
+        // Initialize the ERC721MintAction
+        _initialize(address(mockAsset), MockERC721.mint.selector, mockAsset.mintPrice());
+
+        // Validate the action with a non-existent token
+        vm.expectRevert(ERC721.TokenDoesNotExist.selector);
+        action.validate(LibZip.cdCompress(abi.encode(address(this), 1)));
+    }
+
+    ////////////////////////////////
+    // ERC721MintAction.validated //
+    ////////////////////////////////
+
+    function testValidated() public {
+        // Initialize the ERC721MintAction
+        _initialize(address(mockAsset), MockERC721.mint.selector, mockAsset.mintPrice());
+
+        // Mint a token
+        mockAsset.mint{value: mockAsset.mintPrice()}(address(this));
+        assertTrue(mockAsset.ownerOf(1) == address(this));
+
+        // Validate the action
+        assertTrue(action.validate(LibZip.cdCompress(abi.encode(address(this), 1))));
+
+        // Check the validation status of the token
+        assertTrue(action.validated(1));
+    }
+
+    function testValidated_NotValidated() public {
+        // Initialize the ERC721MintAction
+        _initialize(address(mockAsset), MockERC721.mint.selector, mockAsset.mintPrice());
+
+        // Check the validation status of a non-existent token
+        assertFalse(action.validated(1));
+    }
+
+    ////////////////////////////////////////
+    // ERC721MintAction.supportsInterface //
+    ////////////////////////////////////////
+
+    function testSupportsInterface() public {
+        // Initialize the ERC721MintAction
+        _initialize(address(mockAsset), MockERC721.mint.selector, mockAsset.mintPrice());
+
+        // Check the interface support
+        assertTrue(action.supportsInterface(type(Action).interfaceId));
+        assertTrue(action.supportsInterface(type(Validator).interfaceId));
+    }
+
+    function testSupportsInterface_NotSupported() public {
+        // Initialize the ERC721MintAction
+        _initialize(address(mockAsset), MockERC721.mint.selector, mockAsset.mintPrice());
+
+        // Check the interface support
+        assertFalse(action.supportsInterface(type(ERC721).interfaceId));
+    }
+
+    ///////////////////////////
+    // Test Helper Functions //
+    ///////////////////////////
+
+    function _newActionClone() internal returns (ERC721MintAction) {
+        return ERC721MintAction(address(LibClone.clone(address(new ERC721MintAction()))));
+    }
+
+    function _initialize(address target_, bytes4 selector_, uint256 value_) internal {
+        action.initialize(LibZip.cdCompress(abi.encode(target_, selector_, value_)));
+    }
+}

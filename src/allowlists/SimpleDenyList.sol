4908696cf5a65fcc45f3739bf082fa9be2f60cfc
Chris Cashwell <ccashwell@gmail.com>
2024-03-08 15:07:17 -0500

    feat(allowlist): SimpleDenyList

---
+++ src/allowlists/SimpleDenyList.sol
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.24;
+
+import {LibZip} from "lib/solady/src/utils/LibZip.sol";
+
+import {AllowList} from "src/allowlists/AllowList.sol";
+import {BoostError} from "src/shared/BoostError.sol";
+
+/// @title SimpleDenyList
+/// @notice A simple implementation of an AllowList that implicitly allows all addresses except those explicitly added to the deny list
+contract SimpleDenyList is AllowList {
+    using LibZip for bytes;
+
+    /// @dev An internal mapping of denied statuses
+    mapping(address => bool) private _denied;
+
+    /// @notice Construct a new SimpleDenyList
+    /// @dev Because this contract is a base implementation, it should not be initialized through the constructor. Instead, it should be cloned and initialized using the {initialize} function.
+    constructor() {
+        _disableInitializers();
+    }
+
+    /// @notice Initialize the contract with the initial list of denied addresses
+    /// @param data_ The compressed initialization data `(address owner, address[] denyList)`
+    function initialize(bytes calldata data_) external virtual override initializer {
+        (address owner_, address[] memory denyList_) =
+            abi.decode(data_.cdDecompress(), (address, address[]));
+
+        _initializeOwner(owner_);
+        for (uint256 i = 0; i < denyList_.length; i++) {
+            _denied[denyList_[i]] = true;
+        }
+    }
+
+    /// @notice Check if a user is authorized (i.e. not denied)
+    /// @param user_ The address of the user
+    /// @param - The data payload for the authorization check, not used in this implementation
+    /// @return True if the user is authorized
+    function isAllowed(address user_, bytes calldata /* data_ - unused */ ) external view override returns (bool) {
+        return !_denied[user_];
+    }
+
+    /// @notice Set the denied status of a user
+    /// @param users_ The list of users to update
+    /// @param denied_ The denied status of each user
+    /// @dev The length of the `users_` and `denied_` arrays must be the same
+    /// @dev This function can only be called by the owner
+    function setDenied(address[] calldata users_, bool[] calldata denied_) external onlyOwner {
+        if (users_.length != denied_.length) revert BoostError.LengthMismatch();
+
+        for (uint256 i = 0; i < users_.length; i++) {
+            _denied[users_[i]] = denied_[i];
+        }
+    }
+}
---
+++ test/allowlists/SimpleDenyList.t.sol
@@ -0,0 +1,129 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.24;
+
+import {Test, console} from "lib/forge-std/src/Test.sol";
+
+import {LibClone} from "lib/solady/src/utils/LibClone.sol";
+import {LibZip} from "lib/solady/src/utils/LibZip.sol";
+
+import {BoostError} from "src/shared/BoostError.sol";
+import {Cloneable} from "src/shared/Cloneable.sol";
+import {AllowList} from "src/allowlists/AllowList.sol";
+import {SimpleDenyList} from "src/allowlists/SimpleDenyList.sol";
+
+contract SimpleDenyListTest is Test {
+    SimpleDenyList baseDenyList = new SimpleDenyList();
+    SimpleDenyList denyList;
+
+    function setUp() public {
+        denyList = SimpleDenyList(LibClone.clone(address(baseDenyList)));
+
+        address[] memory users = new address[](2);
+
+        // address(1) and address(3) are denied (all others are allowed)
+        users[0] = address(1);
+        users[1] = address(3);
+
+        // Set the deny list
+        bytes memory data = LibZip.cdCompress(abi.encode(address(this), users));
+        denyList.initialize(data);
+    }
+
+    ////////////////////////////////
+    // SimpleDenyList.initialize //
+    ////////////////////////////////
+
+    function testInitialize() public {
+        SimpleDenyList freshClone = SimpleDenyList(LibClone.clone(address(baseDenyList)));
+        address[] memory users = new address[](1);
+        users[0] = address(1);
+
+        // Ensure the fresh clone is not initialized (no owner or allowlist set)
+        assertEq(freshClone.owner(), address(0));
+        assertTrue(freshClone.isAllowed(address(1), ""));
+
+        // Initialize the fresh clone
+        bytes memory data = LibZip.cdCompress(abi.encode(address(this), users));
+        freshClone.initialize(data);
+
+        // Ensure the fresh clone is initialized
+        assertEq(freshClone.owner(), address(this));
+        assertFalse(freshClone.isAllowed(address(1), ""));
+    }
+
+    function test_InitializerDisabled() public {
+        // Because the slot is private, we use `vm.load` to access it then parse out the bits:
+        //   - [0] is the `initializing` flag (which should be 0 == false)
+        //   - [1..64] hold the `initializedVersion` (which should be 1)
+        bytes32 slot = vm.load(address(denyList), 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffbf601132);
+
+        uint64 version;
+        assembly {
+            version := shr(1, slot)
+        }
+
+        assertNotEq(version, 0, "Version should not be 0");
+    }
+
+    function testInitializer_AlreadyInitialized() public {
+        vm.expectRevert(BoostError.InvalidInitialization.selector);
+        denyList.initialize(LibZip.cdCompress(abi.encode(address(this), new address[](0))));
+    }
+
+    ///////////////////////////////
+    // SimpleDenyList.isAllowed //
+    ///////////////////////////////
+
+    function testIsAllowed() public {
+        assertFalse(denyList.isAllowed(address(1), ""));
+        assertTrue(denyList.isAllowed(address(2), ""));
+        assertFalse(denyList.isAllowed(address(3), ""));
+    }
+
+    function testIsAllowed_UnnecessaryData() public {
+        // Extra data should have no effect on the result because it is ignored in this implementation
+        assertFalse(denyList.isAllowed(address(1), unicode"🦄 unicorns (and 🌈 rainbows!) are *so cool*"));
+        assertTrue(denyList.isAllowed(address(2), abi.encodePacked(uint8(42), keccak256("unexpected"), "data!!1!one1!")));
+    }
+
+    //////////////////////////////
+    // SimpleDenyList.setDenied //
+    //////////////////////////////
+
+    function testSetDenied() public {
+        // Ensure the initially denied addresses are correct
+        assertFalse(denyList.isAllowed(address(1), ""));
+        assertTrue(denyList.isAllowed(address(2), ""));
+        assertFalse(denyList.isAllowed(address(3), ""));
+
+        // Modify the list to set address(2) as denied
+        address[] memory users = new address[](1);
+        bool[] memory denied = new bool[](1);
+        users[0] = address(2);
+        denied[0] = true;
+        denyList.setDenied(users, denied);
+
+        // Ensure the updated deny list is correct
+        assertFalse(denyList.isAllowed(address(1), ""));
+        assertFalse(denyList.isAllowed(address(2), ""));
+        assertFalse(denyList.isAllowed(address(3), ""));
+    }
+
+    function testSetDenied_LengthMismatch() public {
+        vm.expectRevert(BoostError.LengthMismatch.selector);
+        denyList.setDenied(new address[](1), new bool[](2));
+    }
+
+    ///////////////////////////////////////
+    // SimpleDenyList.supportsInterface //
+    ///////////////////////////////////////
+
+    function testSupportsInterface() public {
+        assertTrue(denyList.supportsInterface(type(Cloneable).interfaceId));
+        assertTrue(denyList.supportsInterface(type(AllowList).interfaceId));
+    }
+
+    function testSupportsInterface_Unsupported() public {
+        assertFalse(denyList.supportsInterface(type(Test).interfaceId));
+    }
+}

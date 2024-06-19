c0d077eecb897ba7a8a47d7d8622818524b43f05
Sam McCord <sam.mccord@protonmail.com>
2024-06-19 13:53:36 -0600

    chore(sdk): createBoost generator functionality

--- packages/sdk/src/Deployable/Deployable.ts
+++ packages/sdk/src/Deployable/Deployable.ts
@@ -1,8 +1,13 @@
 import { type Config, deployContract } from '@wagmi/core';
-import type { Address } from 'viem';
+import type { Address, Hex } from 'viem';
 import { DeployableParametersUnspecifiedError } from '../errors';
 
-export type GenericDeployableParams = Parameters<typeof deployContract>[1];
+export type GenericDeployableParams = Omit<
+  Parameters<typeof deployContract>[1],
+  'args'
+> & {
+  args: [Hex, ...Array<Hex>];
+};
 
 export class Deployable {
   protected _address: Address | undefined;
@@ -12,6 +17,10 @@ export class Deployable {
   }
 
   public async deploy(config: Config): Promise<Address> {
+    if (this.address) {
+      // throw? TODO
+      console.warn(`Deployable already has address ${this.address}`);
+    }
     return (this._address = await deployContract(
       config,
       this.buildParameters(config),

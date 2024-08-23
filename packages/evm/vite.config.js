934995e010e5a84fe53ccb30ce2b8acb9ff3ffae
Sam McCord <sam.mccord@protonmail.com>
2024-08-23 15:08:40 -0600

    build(evm,sdk): bundle wagmi codegen correctly

--- packages/evm/vite.config.js
+++ packages/evm/vite.config.js
@@ -2,7 +2,7 @@
 export default {
   build: {
     rollupOptions: {
-      external: [/wagmi/],
+      external: [/^viem/, /^@wagmi(?!.*\/codegen)/],
     },
     lib: {
       entry: ['artifacts/generated.ts'],

b7acfae56fa5c82666938fb445dc11b261714b84
Sam McCord <sam.mccord@protonmail.com>
2024-09-03 11:35:35 -0600

    chore(sdk): update prepareSignerValidatorClaimDataPayload to use interface, proper documentation

--- packages/sdk/src/Incentives/ERC20Incentive.test.ts
+++ packages/sdk/src/Incentives/ERC20Incentive.test.ts
@@ -65,15 +65,15 @@ describe('ERC20Incentive', () => {
     const claimant = trustedSigner.account;
     const incentiveData = pad('0xdef456232173821931823712381232131391321934');
     const incentiveQuantity = 0;
-    const claimDataPayload = await prepareSignerValidatorClaimDataPayload(
-      trustedSigner,
+    const claimDataPayload = await prepareSignerValidatorClaimDataPayload({
+      signer: trustedSigner,
       incentiveData,
-      defaultOptions.config.chains[0].id,
-      boost.validator.assertValidAddress(),
+      chainId: defaultOptions.config.chains[0].id,
+      validator: boost.validator.assertValidAddress(),
       incentiveQuantity,
       claimant,
-      boost.id,
-    );
+      boostId: boost.id,
+    });
 
     await fixtures.core.claimIncentive(
       boost.id,
@@ -109,15 +109,15 @@ describe('ERC20Incentive', () => {
     const claimant = trustedSigner.account;
     const incentiveData = pad('0xdef456232173821931823712381232131391321934');
     const incentiveQuantity = 0;
-    const claimDataPayload = await prepareSignerValidatorClaimDataPayload(
-      trustedSigner,
+    const claimDataPayload = await prepareSignerValidatorClaimDataPayload({
+      signer: trustedSigner,
       incentiveData,
-      defaultOptions.config.chains[0].id,
-      boost.validator.assertValidAddress(),
+      chainId: defaultOptions.config.chains[0].id,
+      validator: boost.validator.assertValidAddress(),
       incentiveQuantity,
       claimant,
-      boost.id,
-    );
+      boostId: boost.id,
+    });
 
     await fixtures.core.claimIncentive(
       boost.id,

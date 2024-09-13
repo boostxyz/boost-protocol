6709f3835d51300be2582b62de7a0b9405335b06
Quazia <alf40k@gmail.com>
2024-09-13 19:54:07 -0400

    fix(sdk): fix bug with fork behavior using mine()
    https://github.com/NomicFoundation/hardhat/issues/5511

--- packages/sdk/test/zora-fork-mint.test.ts
+++ packages/sdk/test/zora-fork-mint.test.ts
@@ -1,8 +1,16 @@
+import { selectors } from '@boostxyz/signatures/events';
 import {
   loadFixture,
-  reset,
+  mine,
 } from '@nomicfoundation/hardhat-toolbox-viem/network-helpers';
-import { type Address, type Hex, parseEther } from 'viem';
+import hre from 'hardhat';
+import {
+  type Address,
+  type Hex,
+  type PublicClient,
+  type WalletClient,
+  parseEther,
+} from 'viem';
 import { beforeAll, beforeEach, describe, expect, test, vi } from 'vitest';
 import { BoostCore } from '../src/BoostCore';
 import {
@@ -19,17 +27,22 @@ import {
   deployFixtures,
   fundBudget,
 } from './helpers';
+import { setupConfig, testAccount } from './viem';
 
 let fixtures: Fixtures, budgets: BudgetFixtures;
 
 describe('Boost with NFT Minting Incentive', () => {
-  const ZORA_CHAIN_URL = process.env.VITE_BASE_MAINNET_RPC;
-  const ZORA_CHAIN_BLOCK = 19694536; // Replace with actual block number
+  // We take the address of the imposter from the transaction above
+  const boostImpostor = '0xE59C9Ca7FFA00471AA2ADA42C0a65C6CaabD06B8' as Address;
 
+  const selector = selectors[
+    'Purchased(address,address,uint256,uint256,uint256)'
+  ] as Hex;
   beforeAll(async () => {
-    await reset(ZORA_CHAIN_URL, ZORA_CHAIN_BLOCK);
+    //await reset(BASE_CHAIN_URL, BASE_CHAIN_BLOCK);
     fixtures = await loadFixture(deployFixtures);
   });
+
   beforeEach(async () => {
     budgets = await loadFixture(fundBudget(defaultOptions, fixtures));
   });
@@ -37,25 +50,33 @@ describe('Boost with NFT Minting Incentive', () => {
   test('should create a boost for incentivizing NFT minting', async () => {
     const { budget, erc20 } = budgets;
     const targetContract = '0xDa8dD2807A33FDA5983F56812765a88B46f0B90B';
+    const walletClient = await hre.viem.getWalletClient(testAccount.address);
 
+    // This is the zora contract we're going to push a transaction against
+    //const contractEntryPoint = '0x9D2FC5fFE5939Efd1d573f975BC5EEFd364779ae';
     const { core, bases } = fixtures;
     const client = new BoostCore({
-      ...defaultOptions,
+      // biome-ignore lint/suspicious/noExplicitAny: The hardhat and wagmi wallet clients are compatible
+      config: setupConfig(walletClient as any),
+      account: testAccount,
       address: core.assertValidAddress(),
     });
     const owner = defaultOptions.account.address;
+    // This is a workaround to this known issue: https://github.com/NomicFoundation/hardhat/issues/5511
+    await mine();
 
     // Step defining the action for Transfer event
     const eventActionStep: ActionStep = {
-      signature: '0xddf252ad', // Transfer(address,address,uint256) event signature
+      signature: selector, // Transfer(address,address,uint256) event signature
       signatureType: SignatureType.EVENT, // We're working with an event
       actionType: 0, // Custom action type (set as 0 for now)
       targetContract: targetContract, // Address of the ERC20 contract
+      // We want to target the Minter property on the Purchase event
       actionParameter: {
         filterType: FilterType.EQUAL, // Filter to check for equality
         fieldType: PrimitiveType.ADDRESS, // The field we're filtering is an address
-        fieldIndex: 0, // Field 0 is the 'from' address in the event
-        filterData: targetContract, // Filtering based on the core address
+        fieldIndex: 1, // Might need to be 2, we'll see - let's log this
+        filterData: boostImpostor, // Filtering based on the core address
       },
     };
 
@@ -63,9 +84,9 @@ describe('Boost with NFT Minting Incentive', () => {
     const eventActionPayload = {
       actionClaimant: {
         signatureType: SignatureType.EVENT,
-        signature: '0xddf252ad' as Hex, // Transfer(address,address,uint256) event signature
+        signature: selector, // Transfer(address,address,uint256) event signature
         fieldIndex: 0, // Targeting the 'from' address
-        targetContract: targetContract as Address, // The ERC20 contract we're monitoring
+        targetContract: boostImpostor, // The ERC20 contract we're monitoring
       },
       actionStepOne: eventActionStep, // Use the custom step for action
       actionStepTwo: eventActionStep, // Repeat the action step if necessary
@@ -79,11 +100,6 @@ describe('Boost with NFT Minting Incentive', () => {
       eventActionPayload,
     );
 
-    console.log('Creating boost with EventAction');
-    console.log('owner:', owner);
-    console.log('budget:', budget);
-    console.log('eventAction:', eventAction);
-    console.log('erc20:', erc20);
     // Create the boost using the custom EventAction
     await client.createBoost({
       protocolFee: 1n,

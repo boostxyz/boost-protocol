bdb43176d82dc22451852a1990f262fbd4f5bfce
mmackz <matthewmackenz@gmail.com>
2024-09-24 16:12:23 -0700

    feat: setup ens commit transaction

--- packages/sdk/test/ens-register.test.ts
+++ packages/sdk/test/ens-register.test.ts
@@ -2,15 +2,16 @@ import { selectors } from '@boostxyz/signatures/events';
 import {
   loadFixture,
   mine,
-  reset,
 } from '@nomicfoundation/hardhat-toolbox-viem/network-helpers';
 import {
   http,
   type Address,
   type Hex,
-  createPublicClient,
+  createTestClient,
   pad,
   parseEther,
+  publicActions,
+  walletActions,
 } from 'viem';
 import { sepolia } from 'viem/chains';
 import { beforeAll, describe, expect, test } from 'vitest';
@@ -50,16 +51,33 @@ const boostImpostor = '0xd11dD72b7555205746fa537928D87DeC6bD26275' as Address;
 const trustedSigner = accounts[0];
 const CHAIN_URL =
   'https://eth-sepolia.g.alchemy.com/v2/' + process.env.VITE_ALCHEMY_API_KEY;
-const CHAIN_BLOCK = 6717979;
+const CHAIN_BLOCK = 6717970n; // block before the commit transaction
 const selector = selectors[
   'NameRegistered(string,bytes32,address,uint256,uint256,uint256)'
 ] as Hex;
 
+// https://sepolia.etherscan.io/tx/0xe527caab33384a58780aa12218c159c0b1910921aca0bc311a9dd7c39efb3316
+const inputData =
+  '0x74694a2b0000000000000000000000000000000000000000000000000000000000000100000000000000000000000000d11dd72b7555205746fa537928d87dec6bd262750000000000000000000000000000000000000000000000000000000007861f809923eb9400000003d38704f2585fb75b2bf96a1c277a5cee490aadc929f777b50000000000000000000000008fade66b79cc9f707ab26799354482eb93a5b7dd000000000000000000000000000000000000000000000000000000000000014000000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000430303030000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000000000000000000000a48b95dd719506df1a6b44c5457e2eff373a7bfa88acc5f9788983f78b6ee96c989d53193c000000000000000000000000000000000000000000000000000000000000003c00000000000000000000000000000000000000000000000000000000000000600000000000000000000000000000000000000000000000000000000000000014d11dd72b7555205746fa537928d87dec6bd2627500000000000000000000000000000000000000000000000000000000000000000000000000000000';
+const commitData =
+  '0xf14fcbc80eaeb279d6cd9d8e9917cb69abaf18adb26cfa42ee7fa24301ac6a915d504f4b';
+
 describe.skipIf(!process.env.VITE_ALCHEMY_API_KEY)(
   'Boost with ENS Registration Incentive',
   () => {
+    const walletClient = createTestClient({
+      transport: http('http://127.0.0.1:8545'),
+      chain: sepolia,
+      mode: 'hardhat',
+    })
+      .extend(publicActions)
+      .extend(walletActions);
+
     beforeAll(async () => {
-      await reset(CHAIN_URL, CHAIN_BLOCK);
+      await walletClient.reset({
+        jsonRpcUrl: CHAIN_URL,
+        blockNumber: CHAIN_BLOCK,
+      });
       fixtures = await loadFixture(deployFixtures);
       budgets = await loadFixture(fundBudget(defaultOptions, fixtures));
     });
@@ -140,19 +158,39 @@ describe.skipIf(!process.env.VITE_ALCHEMY_API_KEY)(
       const action = boost.action;
       expect(action).toBeDefined();
 
-      // Fetch logs for historic transaction
-      const sepoliaClient = createPublicClient({
-        chain: sepolia,
-        transport: http(CHAIN_URL),
+      await walletClient.impersonateAccount({
+        address: boostImpostor,
+      });
+      await walletClient.setBalance({
+        address: boostImpostor,
+        value: parseEther('10'),
       });
 
-      const logs = await sepoliaClient.getLogs({
-        address: targetContract as Address,
-        fromBlock: BigInt(CHAIN_BLOCK), // target the exact block of the transaction
-        toBlock: BigInt(CHAIN_BLOCK),
+      // submit the commit transaction
+      const commitReceipt = await walletClient.sendTransaction({
+        data: commitData,
+        account: boostImpostor,
+        to: targetContract,
+        value: parseEther('0'),
+      });
+      expect(commitReceipt).toBeDefined();
+
+      // allow for 60 seconds to pass before registering ENS name
+      await walletClient.request({
+        method: 'evm_increaseTime',
+        params: ['0x3c'], // increase time by 60 seconds
       });
 
-      const validation = await action.validateActionSteps({ logs });
+      const registerReceipt = await walletClient.sendTransaction({
+        data: inputData,
+        account: boostImpostor,
+        to: targetContract,
+        value: parseEther('0.408279452054767655'),
+      });
+      // Make sure that the transaction was sent as expected and validates the action
+      expect(registerReceipt).toBeDefined();
+
+      const validation = await action.validateActionSteps();
       expect(validation).toBe(true);
       // Generate the signature using the trusted signer
       const claimDataPayload = await prepareSignerValidatorClaimDataPayload({

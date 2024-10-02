0e0639e115aa04afa921ea61288fa3048edc6415
Jonathan Diep <jonathandiep@users.noreply.github.com>
2024-10-02 14:12:17 -0700

    test: add agora vote example

---
+++ examples/agora-vote/vitest.config.ts
@@ -0,0 +1,15 @@
+import { loadEnv } from 'vite';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  define: {
+    __DEFAULT_CHAIN_ID__: 10,
+  },
+  test: {
+    fileParallelism: false,
+    env: loadEnv('', process.cwd(), ''),
+    globalSetup: ['../../test/src/setup.hardhat.ts'],
+    hookTimeout: 30_000,
+    testTimeout: 15_000,
+  },
+});

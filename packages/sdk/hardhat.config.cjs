051595c5af3fb21c56184d0930d510f922be6923
mmackz <matthewmackenz@gmail.com>
2024-10-03 20:29:10 -0700

    chore: update hardhat config in sdk so tests work

--- packages/sdk/hardhat.config.cjs
+++ packages/sdk/hardhat.config.cjs
@@ -1,20 +1,7 @@
-require('dotenv').config();
-
 module.exports = {
   networks: {
-    ...(process.env.VITE_ALCHEMY_API_KEY
-      ? {
-          hardhat: {
-            // We might not need the mine() function if we use this code https://github.com/NomicFoundation/hardhat/pull/5394/files
-            chainId: 8453, // Base chain ID
-            //hardfork: 'cancun',
-            forking: {
-              url:
-                'https://base-mainnet.g.alchemy.com/v2/' +
-                process.env.VITE_ALCHEMY_API_KEY,
-            },
-          },
-        }
-      : {}),
+    hardhat: {
+      chainId: 31337,
+    },
   },
 };

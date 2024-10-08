555c318494b9d0a1c447ca2f831e33d564213039
Quazia <alf40k@gmail.com>
2024-10-08 13:12:55 -0400

    refactor(sdk): change inheritance for Variable Critiria Incentive
    I ran into issues when trying to inherit from another incentive when overriding the ABI and trying to pass through the payload type. This fixes it but it's crude.

--- packages/sdk/src/Incentives/ERC20VariableCriteriaIncentive.ts
+++ packages/sdk/src/Incentives/ERC20VariableCriteriaIncentive.ts
@@ -1,7 +1,8 @@
 import {
-  type erc20VariableCriteriaIncentiveAbi,
+  erc20VariableCriteriaIncentiveAbi,
   readErc20VariableCriteriaIncentiveGetIncentiveCriteria,
 } from '@boostxyz/evm';
+import { bytecode } from '@boostxyz/evm/artifacts/contracts/incentives/ERC20VariableCriteriaIncentive.sol/ERC20VariableCriteriaIncentive.json';
 import events from '@boostxyz/signatures/events';
 import functions from '@boostxyz/signatures/functions';
 import {
@@ -11,12 +12,74 @@ import {
   type Hex,
   type Log,
   decodeFunctionData,
+  encodeAbiParameters,
   parseEventLogs,
 } from 'viem';
+import { ERC20VariableCriteriaIncentive as ERC20VariableCriteriaIncentiveBases } from '../../dist/deployments.json';
+
 import { getTransaction, getTransactionReceipt } from 'viem/actions';
 import { SignatureType } from '../Actions/EventAction';
+import type {
+  DeployableOptions,
+  GenericDeployableParams,
+} from '../Deployable/Deployable';
+import { DeployableTarget } from '../Deployable/DeployableTarget';
 import type { ReadParams } from '../utils';
 import { ERC20VariableIncentive } from './ERC20VariableIncentive';
+
+export interface ERC20VariableCriteriaIncentivePayload {
+  /**
+   * The address of the incentivized asset.
+   *
+   * @type {Address}
+   */
+  asset: Address;
+  /**
+   * The amount of the asset to distribute as reward.
+   *
+   * @type {bigint}
+   */
+  reward: bigint;
+  /**
+   * The total spending limit of the asset that will be distributed.
+   *
+   * @type {bigint}
+   */
+  limit: bigint;
+  /**
+   * The criteria for the incentive that determines how the reward is distributed.
+   *
+   * @type {IncentiveCriteria}
+   */
+  criteria: IncentiveCriteria;
+}
+
+export interface IncentiveCriteria {
+  /**
+   * The type of criteria used, either function signature or event signature.
+   *
+   * @type {SignatureType}
+   */
+  criteriaType: SignatureType;
+  /**
+   * The function or event signature used for criteria matching.
+   *
+   * @type {Hex}
+   */
+  signature: Hex;
+  /**
+   * The index of the field from where the scalar value is extracted.
+   *
+   * @type {number}
+   */
+  fieldIndex: number;
+  /**
+   * The address of the contract where the event/function is called/emitted.
+   *
+   * @type {Address}
+   */
+  targetContract: Address;
+}
 
 // Note: Move these to a separate file and refine
 class IncentiveCriteriaNotFoundError extends Error {
@@ -47,13 +110,6 @@ class DecodedArgsError extends Error {
   }
 }
 
-export interface IncentiveCriteria {
-  criteriaType: SignatureType;
-  signature: Hex; // event/function signature selector
-  fieldIndex: number; // the index in the decoded args where the scalar resides
-  targetContract: Address; // the contract address to match
-}
-
 export interface ReadIncentiveCriteriaParams
   extends ReadParams<
     typeof erc20VariableCriteriaIncentiveAbi,
@@ -67,7 +123,26 @@ export interface GetIncentiveScalarParams {
 /**
  * Extended ERC20 Variable Criteria Incentive class that fetches incentive criteria and scalar
  */
-export class ERC20VariableCriteriaIncentive extends ERC20VariableIncentive {
+export class ERC20VariableCriteriaIncentive extends DeployableTarget<
+  ERC20VariableCriteriaIncentivePayload,
+  typeof erc20VariableCriteriaIncentiveAbi
+> {
+  public override readonly abi = erc20VariableCriteriaIncentiveAbi;
+  /**
+   * @inheritdoc
+   *
+   * @public
+   * @static
+   * @type {Record<number, Address>}
+   */
+  public static override bases: Record<number, Address> = {
+    ...(ERC20VariableCriteriaIncentiveBases as Record<number, Address>),
+  };
+
+  /**
+   *Functions from the ERC20VariableIncentive contract
+   */
+
   /**
    * Fetches the IncentiveCriteria struct from the contract
    *
@@ -181,4 +256,81 @@ export class ERC20VariableCriteriaIncentive extends ERC20VariableIncentive {
       );
     }
   }
+  /**
+   * @inheritdoc
+   *
+   * @public
+   * @param {?ERC20VariableCriteriaIncentivePayload} [_payload]
+   * @param {?DeployableOptions} [_options]
+   * @returns {GenericDeployableParams}
+   */
+  public override buildParameters(
+    _payload?: ERC20VariableCriteriaIncentivePayload,
+    _options?: DeployableOptions,
+  ): GenericDeployableParams {
+    const [payload, options] = this.validateDeploymentConfig(
+      _payload,
+      _options,
+    );
+    return {
+      abi: erc20VariableCriteriaIncentiveAbi,
+      bytecode: bytecode as Hex,
+      args: [prepareERC20VariableCriteriaIncentivePayload(payload)],
+      ...this.optionallyAttachAccount(options.account),
+    };
+  }
+}
+
+/**
+ *
+ *
+ * @param {InitPayloadExtended} param0
+ * @param {Address} param0.asset - The address of the ERC20 asset to incentivize.
+ * @param {bigint} param0.reward - The reward amount to distribute per action.
+ * @param {bigint} param0.limit - The total limit of the asset distribution.
+ * @param {IncentiveCriteria} param0.criteria - The incentive criteria for reward distribution.
+ * @returns {Hex}
+ */
+export function prepareERC20VariableCriteriaIncentivePayload({
+  asset,
+  reward,
+  limit,
+  criteria,
+}: ERC20VariableCriteriaIncentivePayload) {
+  return encodeAbiParameters(
+    [
+      {
+        type: 'tuple',
+        name: 'initPayloadExtended',
+        components: [
+          { type: 'address', name: 'asset' },
+          { type: 'uint256', name: 'reward' },
+          { type: 'uint256', name: 'limit' },
+          {
+            type: 'tuple',
+            name: 'criteria',
+            components: [
+              { type: 'uint8', name: 'criteriaType' },
+              { type: 'bytes32', name: 'signature' },
+              { type: 'uint8', name: 'fieldIndex' },
+              { type: 'address', name: 'targetContract' },
+            ],
+          },
+        ],
+      },
+    ],
+    [
+      {
+        asset: asset,
+        reward: reward,
+        limit: limit,
+        criteria: {
+          criteriaType: criteria.criteriaType,
+          signature: criteria.signature,
+          fieldIndex: criteria.fieldIndex,
+          targetContract: criteria.targetContract,
+        },
+      },
+    ],
+  );
 }

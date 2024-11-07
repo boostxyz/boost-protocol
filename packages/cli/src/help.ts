6d53d3deb163ad43615c3c2ef6b50a792b1fc656
topocount <17910833+topocount@users.noreply.github.com>
2024-11-07 16:57:37 -0600

    Apply suggestions from code review
    Co-authored-by: Sam McCord <sam.mccord@protonmail.com>

--- packages/cli/src/help.ts
+++ packages/cli/src/help.ts
@@ -5,7 +5,8 @@ Usage: boost <COMMAND>
 
 Commands:
   deploy                  Deploy a new version of the protocol
-  seed generate           Generate a new basic Boost seed to stdout
+  seed generate           Generate a new basic Boost seed
+  seed erc20              Deploys a new ERC20
   seed first.json [...]
 
 Options:
@@ -15,6 +16,7 @@ Options:
   --chain        Viem chain name, see https://github.com/wevm/viem/tree/main/src/chains/definitions for a list of valid names
   --privateKey   Private key to derive account from to use for contract operations, see https://viem.sh/docs/accounts/local/privateKeyToAccount#generating-private-keys for more information
   --mnemonic     Mnemonic to derive account from to use for contract operations, see https://viem.sh/docs/accounts/local/mnemonicToAccount#generating-private-keys for more information
+  --rpcUrl       Optional URL to provide to Viem http transport
   --out          Path to file to output command results
   --cacheDir     Path to artifact cache directory
   --force        Bypass cache for given command and configuration, re-running any tasks and re-generating cache artifacts

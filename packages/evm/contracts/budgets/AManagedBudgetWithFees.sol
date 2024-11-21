9921975775db59aeeeb7228b3fb16fa725b69392
Kevin Siegler <17910833+topocount@users.noreply.github.com>
2024-11-21 15:23:38 -0600

    feat(evm): add combined function for clawbacks and fee payouts

--- packages/evm/contracts/budgets/AManagedBudgetWithFees.sol
+++ packages/evm/contracts/budgets/AManagedBudgetWithFees.sol
@@ -38,6 +38,28 @@ abstract contract AManagedBudgetWithFees is AManagedBudget {
         uint256 indexed questId, uint256 indexed incentiveId, address indexed manager, uint256 amount
     );
 
+    /// @notice Pays the management fee for a specific boost and incentive
+    /// @param boostId The ID of the boost for which the management fee is being paid
+    /// @param incentiveId The ID of the incentive within the boost
+    /// @dev The function checks the type of incentive and ensures that the claims have reached the limit
+    /// or the balance is zero before transferring the management fee to the boost owner
+    /// @dev Supports management fee payouts for {@link AERC20Incentive} and {@link AERC20VariableIncentive} deployments
+    function payManagementFee(uint256 boostId, uint256 incentiveId) external virtual;
+
+    /// @notice Claws back assets from a target and pays the management fee for a specific boost and incentive
+    /// @param target The address of the target from which assets are being clawed back
+    /// @param data_ The packed data for the clawback request
+    /// @param boostId The ID of the boost
+    /// @param incentiveId The ID of the incentive within the boost for which the management fee is being paid
+    /// @return amount The amount of assets clawed back
+    /// @return asset The address of the asset clawed back
+    /// @dev This function first claws back assets from the target and then pays
+    /// the management fee for the specified boost and incentive
+    function clawbackFromTargetAndPayFee(address target, bytes calldata data_, uint256 boostId, uint256 incentiveId)
+        external
+        virtual
+        returns (uint256 amount, address asset);
+
     /// @inheritdoc ACloneable
     function supportsInterface(bytes4 interfaceId) public view virtual override(AManagedBudget) returns (bool) {
         return interfaceId == type(AManagedBudgetWithFees).interfaceId || super.supportsInterface(interfaceId);

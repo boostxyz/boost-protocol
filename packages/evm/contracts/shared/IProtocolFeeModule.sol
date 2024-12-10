6960d45ca1cf296f85fcccc967ca9be0b96ff04f
Quazia <alf40k@gmail.com>
2024-12-10 11:20:42 -0500

    feat(evm): add protocol fee token to core

--- packages/evm/contracts/shared/IProtocolFeeModule.sol
+++ packages/evm/contracts/shared/IProtocolFeeModule.sol
@@ -8,4 +8,8 @@ interface IProtocolFeeModule {
     /// @notice Returns the protocol fee
     /// @return The protocol fee as a uint64
     function getProtocolFee(bytes calldata data_) external view returns (uint64);
+
+    /// @notice Returns the protocol asset to be used based on the boost configuration
+    /// @return The protocol asset address
+    function getProtocolAsset(bytes calldata data_) external view returns (address);
 }

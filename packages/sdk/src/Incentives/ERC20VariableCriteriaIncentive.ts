b54d6e18d9401736630c0527db1683e2c4be7698
Jonathan Diep <jonathandiep@users.noreply.github.com>
2025-03-04 14:55:42 -0800

    feat: add `decodeClaimData` for `ERC20VariableCritieriaIncentive`

--- packages/sdk/src/Incentives/ERC20VariableCriteriaIncentive.ts
+++ packages/sdk/src/Incentives/ERC20VariableCriteriaIncentive.ts
@@ -10,8 +10,10 @@ import {
   type AbiFunction,
   type Address,
   type Hex,
+  decodeAbiParameters,
   decodeFunctionData,
   encodeAbiParameters,
+  parseEther,
   parseEventLogs,
   zeroAddress,
   zeroHash,
@@ -156,6 +158,51 @@ export class ERC20VariableCriteriaIncentive extends ERC20VariableIncentive<
     } catch (e) {
       throw new IncentiveCriteriaNotFoundError(e as Error);
     }
+  }
+
+  /**
+   * Decodes claim data for the ERC20VariableCriteriaIncentive, returning the claim amount.
+   * Useful when deriving amount claimed from logs.
+   *
+   * @public
+   * @param {Hex} claimData
+   * @returns {Promise<bigint>} Returns the reward amount from a claim data payload
+   */
+  public override async decodeClaimData(claimData: Hex) {
+    const boostClaimData = decodeAbiParameters(
+      [
+        {
+          type: 'tuple',
+          name: 'BoostClaimData',
+          components: [
+            { type: 'bytes', name: 'validatorData' },
+            { type: 'bytes', name: 'incentiveData' },
+          ],
+        },
+      ],
+      claimData,
+    );
+    const signedAmount = decodeAbiParameters(
+      [{ type: 'uint256' }],
+      boostClaimData[0].incentiveData,
+    )[0];
+    let claimAmount = signedAmount;
+    const [reward, maxReward] = await Promise.all([
+      this.reward(),
+      this.getMaxReward(),
+    ]);
+
+    if (reward === 0n) {
+      return claimAmount;
+    } else {
+      claimAmount = (reward * signedAmount) / parseEther('1');
+    }
+
+    if (maxReward !== 0n && claimAmount > maxReward) {
+      claimAmount = maxReward;
+    }
+
+    return claimAmount;
   }
 
   /**

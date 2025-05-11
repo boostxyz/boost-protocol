52e07b2eb41302c96c789eb565410298a180253b
mmackz <matthewmackenz@gmail.com>
2025-05-10 18:44:18 -0700

    refactor: pad signature in variable criteria incentives

--- packages/sdk/src/Incentives/ERC20VariableCriteriaIncentiveV2.ts
+++ packages/sdk/src/Incentives/ERC20VariableCriteriaIncentiveV2.ts
@@ -13,6 +13,7 @@ import {
   decodeAbiParameters,
   decodeFunctionData,
   encodeAbiParameters,
+  pad,
   parseEther,
   parseEventLogs,
   zeroAddress,
@@ -456,7 +457,7 @@ export function prepareERC20VariableCriteriaIncentiveV2Payload({
         maxReward: maxReward,
         criteria: {
           criteriaType: criteria.criteriaType,
-          signature: criteria.signature,
+          signature: pad(criteria.signature),
           fieldIndex: criteria.fieldIndex,
           targetContract: criteria.targetContract,
           valueType: criteria.valueType,

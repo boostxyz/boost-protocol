c8a892bc8d32ac2028fb14b9eccb84339053c091
Jonathan Diep <jonathandiep@users.noreply.github.com>
2025-09-24 14:57:56 -0700

    feat(evm): add new validators to handle referrals in claim data

---
+++ packages/evm/contracts/validators/APayableLimitedSignerValidatorV2.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.24;
+
+import {SignatureCheckerLib} from "@solady/utils/SignatureCheckerLib.sol";
+
+import {BoostError} from "contracts/shared/BoostError.sol";
+import {ACloneable} from "contracts/shared/ACloneable.sol";
+
+import {ALimitedSignerValidatorV2} from "contracts/validators/ALimitedSignerValidatorV2.sol";
+
+/// @title Payable Limited Signer Validator V2
+/// @notice A simple implementation of a Validator that verifies a given signature and checks the recovered address against a set of authorized signers.
+/// @dev The claim fee is stored on the base implementation and read by all clones. Fee is forwarded to the protocol fee receiver from BoostCore.
+abstract contract APayableLimitedSignerValidatorV2 is ALimitedSignerValidatorV2 {
+    /// @notice The claim fee required to validate a claim (only meaningful on base implementation)
+    uint256 public claimFee;
+
+    /// @notice Emitted when the claim fee is updated
+    event ClaimFeeUpdated(uint256 newFee);
+
+    /// @notice Emitted when a claim fee is paid
+    event ClaimFeePaid(
+        address indexed claimant, uint256 indexed boostId, uint256 indexed incentiveId, uint256 fee, address feeReceiver
+    );
+
+    /// @notice Thrown when the provided fee does not match the required claim fee
+    error InvalidClaimFee();
+
+    /// @notice Thrown when fee transfer fails
+    error FeeTransferFailed();
+
+    /// @inheritdoc ACloneable
+    function getComponentInterface() public pure virtual override returns (bytes4) {
+        return type(APayableLimitedSignerValidatorV2).interfaceId;
+    }
+
+    /// @inheritdoc ACloneable
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        virtual
+        override(ALimitedSignerValidatorV2)
+        returns (bool)
+    {
+        return interfaceId == type(APayableLimitedSignerValidatorV2).interfaceId || super.supportsInterface(interfaceId);
+    }
+}
---
+++ packages/evm/contracts/validators/SignerValidatorV2.sol
@@ -0,0 +1,132 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.24;
+
+import {Ownable} from "@solady/auth/Ownable.sol";
+
+import {SignatureCheckerLib} from "@solady/utils/SignatureCheckerLib.sol";
+import {EIP712} from "@solady/utils/EIP712.sol";
+
+import {ACloneable} from "contracts/shared/ACloneable.sol";
+import {BoostError} from "contracts/shared/BoostError.sol";
+import {IncentiveBits} from "contracts/shared/IncentiveBits.sol";
+
+import {AValidator} from "contracts/validators/AValidator.sol";
+import {ASignerValidatorV2} from "contracts/validators/ASignerValidatorV2.sol";
+
+/// @title Signer Validator V2
+/// @notice Enhanced implementation that includes referrer verification in signed claims
+/// @dev This version requires the referrer to be included in the signed claim data
+contract SignerValidatorV2 is ASignerValidatorV2, Ownable, EIP712 {
+    using SignatureCheckerLib for address;
+    using IncentiveBits for IncentiveBits.IncentiveMap;
+
+    event SignerValidatorV2Initialized(address validatorCaller);
+
+    /// @dev track claimed incentives using this bitmap
+    IncentiveBits.IncentiveMap _used;
+
+    /// @dev address allowed to call validate
+    address internal _validatorCaller;
+
+    bytes32 internal constant _SIGNER_VALIDATOR_TYPEHASH = keccak256(
+        "SignerValidatorData(uint256 boostId,uint8 incentiveQuantity,address claimant,bytes incentiveData,address referrer)"
+    );
+
+    /// @notice Construct a new SignerValidatorV2
+    /// @dev Because this contract is a base implementation, it should not be initialized through the constructor. Instead, it should be cloned and initialized using the {initialize} function.
+    constructor() {
+        _disableInitializers();
+    }
+
+    /// @notice Initialize the contract with the list of authorized signers
+    /// @param data_ The compressed list of authorized signers
+    /// @dev The first address in the list will be the initial owner of the contract
+    function initialize(bytes calldata data_) public virtual override initializer {
+        (address[] memory signers_, address validatorCaller_) = abi.decode(data_, (address[], address));
+        _initializeOwner(signers_[0]);
+        _validatorCaller = validatorCaller_;
+        for (uint256 i = 0; i < signers_.length; i++) {
+            signers[signers_[i]] = true;
+        }
+        emit SignerValidatorV2Initialized(validatorCaller_);
+    }
+
+    /// Validate that the action has been completed successfully by constructing a payload and checking the signature against it
+    /// @inheritdoc AValidator
+    function validate(uint256 boostId, uint256 incentiveId, address claimant, bytes calldata claimData)
+        public
+        payable
+        virtual
+        override
+        returns (bool)
+    {
+        if (msg.sender != _validatorCaller) revert BoostError.Unauthorized();
+
+        (BoostClaimDataWithReferrer memory claim) = abi.decode(claimData, (BoostClaimDataWithReferrer));
+        (SignerValidatorInputParams memory validatorData) =
+            abi.decode(claim.validatorData, (SignerValidatorInputParams));
+
+        bytes32 hash =
+            hashSignerData(boostId, validatorData.incentiveQuantity, claimant, claim.incentiveData, claim.referrer);
+
+        if (uint256(validatorData.incentiveQuantity) <= incentiveId) {
+            revert BoostError.InvalidIncentive(validatorData.incentiveQuantity, incentiveId);
+        }
+        if (!signers[validatorData.signer]) revert BoostError.Unauthorized();
+
+        // Mark the incentive as claimed to prevent replays
+        // checks internally if the incentive has already been claimed
+        _used.setOrThrow(hash, incentiveId);
+
+        // Return the result of the signature check
+        // no need for a sig prefix since it's encoded by the EIP712 lib
+        return validatorData.signer.isValidSignatureNow(hash, validatorData.signature);
+    }
+
+    /// @notice Set the authorized status of a signer
+    /// @param signers_ The list of signers to update
+    /// @param authorized_ The authorized status of each signer
+    function setAuthorized(address[] calldata signers_, bool[] calldata authorized_) external override onlyOwner {
+        if (signers_.length != authorized_.length) revert BoostError.LengthMismatch();
+
+        for (uint256 i = 0; i < signers_.length; i++) {
+            signers[signers_[i]] = authorized_[i];
+        }
+    }
+
+    /// @inheritdoc ASignerValidatorV2
+    function setValidatorCaller(address newCaller) external override onlyOwner {
+        _validatorCaller = newCaller;
+    }
+
+    function hashSignerData(
+        uint256 boostId,
+        uint8 incentiveQuantity,
+        address claimant,
+        bytes memory incentiveData,
+        address referrer
+    ) public view returns (bytes32 hashedSignerData) {
+        return _hashTypedData(
+            keccak256(
+                abi.encode(
+                    _SIGNER_VALIDATOR_TYPEHASH, boostId, incentiveQuantity, claimant, keccak256(incentiveData), referrer
+                )
+            )
+        );
+    }
+
+    function _domainNameAndVersion()
+        internal
+        pure
+        virtual
+        override
+        returns (string memory name, string memory version)
+    {
+        name = "SignerValidatorV2";
+        version = "1";
+    }
+
+    function _domainNameAndVersionMayChange() internal pure override returns (bool result) {
+        result = true;
+    }
+}
